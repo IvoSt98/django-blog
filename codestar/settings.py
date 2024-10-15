5861e2ef5a6a43eea57fe5e17c4cd68fbc6d86ca
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-10-15 01:00:23 +0000

    :

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-ivost98-djangoblog-wdr429upwdw.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -47,11 +47,11 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'blog',
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
     'cloudinary',
+    'blog',
     'about',
 ]
 
